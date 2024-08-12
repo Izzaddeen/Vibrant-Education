e9fa4325037170ded4426ffec8eff43c94a234d8
Izzaddeen <izzarex05@gmail.com>
2024-08-12 17:11:58 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,39 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./Components/footer";
 import Navbar from "./Components/navbar";
 import Home from "./Pages/home";
-import About from "./Pages/about";
-import Services from "./Pages/services";
-import WhyChooseUs from "./Pages/whychooseus";
-import ClientSays from "./Pages/clientsays";
-import ContactForm from "./Pages/contact";
-import Youtube from "./Pages/youtube";
 
 function App() {
   return (
-      <>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/youtube" element={<Youtube />} />
-        <Route path="/why-choose-us" element={<WhyChooseUs />} />
-        <Route path="/client-says" element={<ClientSays />} />
-        <Route path="/contact" element={<ContactForm />} /> */}
+        {/* Uncomment the following routes as needed */}
+        {/* <Route path="/about" element={<About />} /> */}
+        {/* <Route path="/services" element={<Services />} /> */}
+        {/* <Route path="/blog" element={<Blog />} /> */}
+        {/* <Route path="/contact" element={<ContactForm />} /> */}
       </Routes>
       <Footer />
-      </>
+    </>
   );
 }
 
 export default App;
-
-
-      {/* <Navbar /> */}
-      {/* <Home /> */}
-         {/* <Services /> */}
-      {/* <Youtube /> */}
-      {/* <WhyChooseUs /> */}
-      {/* <ClientSays /> */}
-      {/* <ContactForm /> */}
-      {/* <Footer /> */}
--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,91 +1,32 @@
-import React from "react";
+import React, { useState } from 'react';
 import '../Components/navbar.css'
-import { useState} from "react";
-import MobileNav from "./mobileNav";
-import { FaTimes, FaBars } from 'react-icons/fa';
-import logo from '../Images/logo.png';
-import { Link } from "react-router-dom";
-
-
-
+import { FaBars, FaTimes } from 'react-icons/fa';
+import vibee from '../Images/logo.png';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
 
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const toggleMenu = () => {
-      setIsMenuOpen(!isMenuOpen);
+    const handleToggle = () => {
+        setIsOpen(!isOpen);
     };
-  
 
     return (
-        <>
-        <div className="nav-section">
-            <div className="nav-contents-1">
-                <ul className="nav-ul">
-                    <li className="nav-li">
-                        <i className="fa-solid fa-phone"></i>
-                        <p>+91 98427 44441 - India</p>
-                    </li>
-                    <li className="nav-li">
-                        <i className="fa-solid fa-phone"></i>
-                        <p>+603-7877 2553 - Malaysia</p>
-                    </li>
-                </ul>
+        <nav className="navbar bg-light">
+            <div className="nav-logo">
+                <img src={vibee} alt="Logo" style={{ height: '40px' }} />
             </div>
-            <div className="nav-contents-2">
-                <ul className="nav-ul">
-                    <li className="nav-li">
-                        <p>......................</p>
-                    </li>
-                    <li className="nav-li">
-                        <i className="fa-brands fa-facebook"></i>
-                        <i className="fa-brands fa-twitter"></i>
-                        <i className="fa-brands fa-square-instagram"></i>
-                    </li>
-                </ul>
+            <div className={`nav-menu ${isOpen ? 'open' : ''}`}>
+                <a href="#home" onClick={() => setIsOpen(false)}>Home</a>
+                <a href="#about" onClick={() => setIsOpen(false)}>About Us</a>
+                <a href="#services" onClick={() => setIsOpen(false)}>Services</a>
+                <a href="#blog" onClick={() => setIsOpen(false)}>Blog</a>
+                <a href="#contact" onClick={() => setIsOpen(false)}>Contact Us</a>
             </div>
-        </div>
-        <hr></hr>
-
-        {/* NAVBAR */}
-
-        <MobileNav isOpen={isMenuOpen} toggleMenu={toggleMenu} />
-      <nav className='navbar-container'>
-        <div className='navbar-content'>
-          <img className='nav-logo' src={logo} alt='' />
-          <ul>
-            <li>
-              <Link className='menu-item' to="/">HOME</Link>
-            </li>
-            <li>
-              <Link className='menu-item' to="/about">ABOUT US</Link>
-            </li>
-            <li>
-              <Link className='menu-item' to="/services">SERVICES</Link>
-            </li>
-            <li>
-              <Link className='menu-item' to="/blog">BLOG</Link>
-            </li>
-            <li>
-              <Link className='menu-item' to="/contact">CONTACT US</Link>
-            </li>
-          </ul>
-          <button
-            className='menu-btn'
-            onClick={toggleMenu}
-            style={{ color: 'white', background: 'none', border: 'none', cursor: 'pointer' }}
-          >
-            {isMenuOpen ? <FaTimes size="1.8rem" /> : <FaBars size="1.8rem" />}
-          </button>
-        </div>
-      </nav>
-
-        </>
+            <div className="nav-toggle" onClick={handleToggle}>
+                {isOpen ? <FaTimes /> : <FaBars />}
+            </div>
+        </nav>
     );
-}
+};
 
 export default Navbar;
-
-
-
